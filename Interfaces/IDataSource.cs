5558a0cc116efa5406b0e83d6d699bd537be188f
MaxGsomGsom <max_kuzmin@outlook.com>
2018-01-24 19:44:23 +0300

    Created projects structure

---
+++ Interfaces/IDataSource.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MentolDataImporter.Interfaces
+{
+    interface IDataSource
+    {
+        List<string[]> Parse(List<string> data);
+    }
+}
